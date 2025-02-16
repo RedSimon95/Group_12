b6fc86c6b487cc8bf6beee8304a980381fb815f0
RedSimon95 <simone.fiorini@alcampus.it>
2025-02-16 19:36:37 +0100

    Metodo Holdout implementato

--- evaluation.py
+++ evaluation.py
@@ -19,3 +19,23 @@ class ModelEvaluator:
         self.y = np.array(y).flatten()
         self.logs = []  # Creo struttura per il logging , utile per i successivi metodi che verranno
       
+    
+    def holdout(self, test_size=0.2):
+        """
+        Divide i dati in training e test set secondo la percentuale specificata e valuta il modello.
+        """
+        n_test = int(len(self.X) * test_size)
+        indices = np.random.permutation(len(self.X))
+        test_indices, train_indices = indices[:n_test], indices[n_test:]
+        
+        X_train, X_test = self.X[train_indices], self.X[test_indices]
+        y_train, y_test = self.y[train_indices], self.y[test_indices]
+        
+        self.model.fit(X_train, y_train)
+        y_pred = self.model.predict(X_test)
+        
+        results = self.compute_metrics(y_test, y_pred)
+        self.logs.append({"Method": "Holdout", **results})
+        self._update_confusion_matrix(y_test, y_pred)
+        self._plot_confusion_matrix()
+        return results
